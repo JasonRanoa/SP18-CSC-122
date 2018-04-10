7a302126fbea79f8be0598b2fcf7017f1f3ecfdf
JasonRanoa <jasonranoa1102@gmail.com>
2018-04-10 14:31:53 -0500

    .

--- Project Files/Ch17.Pr11.RunningBack.vMac/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <ncurses.h>
-
-int main() {
-	initscr();
-	std::cout << "Hello, World!" << std::endl;
-	endwin();
-}
--- Project Files/Ch17.Pr13.Running-Back.vMac-Failed/ImageMap.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by TheLoneWoof on 3/26/18.
-//
-
-#include "ImageMap.h"
-
-void ImageMap::add(Coord coord)
