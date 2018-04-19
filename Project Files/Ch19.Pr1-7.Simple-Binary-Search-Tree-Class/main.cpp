3f362a7e79b99e493f604a600a3354b219ebe068
JasonRanoa <jasonranoa1102@gmail.com>
2018-04-19 15:07:49 -0500

    Final Project

--- Project Files/Ch19.Pr1-7.Simple-Binary-Search-Tree-Class/main.cpp
+++ Project Files/Ch19.Pr1-7.Simple-Binary-Search-Tree-Class/main.cpp
@@ -1,62 +1,155 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include "BinaryTree.h"
 
+void enterTestData(BinaryTree &);
+void displayTreeInfo(BinaryTree * const, std::string);
+
+void demoOne();
+void demoTwo();
+void demoThree();
+
 int main() {
-    BinaryTree bin;
-
-    bin.insert(2.34);
-    bin.insert(3.14);
-    bin.insert(6.28);
-    bin.insert(2.98);
-    bin.insert(1.21);
-    bin.insert(-0.54);
-    bin.insert(100.0);
-    bin.insert(100.4);
-    bin.insert(100.5);
-    bin.insert(100.6);
-    bin.insert(0);
-
-    std::vector<double> list;
-    bin.inorder(list);
-
-
-    for (double d : list) {
-        std::cout << d << " ";
-    }
-    std::cout << "\n";
-
-    std::cout << "SIZE: " << bin.size() << "\n";
-    std::cout << "LEAVES: " << bin.leafCount() << "\n";
-    std::cout << "HEIGHT: " << bin.height() << "\n";
-    std::cout << "WIDTH: " << bin.width() << "\n";
-
-    BinaryTree vin = bin;
-
-    list.clear();
-    vin.inorder(list);
-
-    for (double d : list) {
-        std::cout << d << " ";
-    }
-    std::cout << "\n";
-
-    vin.insert(40);
-    bin = vin;
-
-    std::cout << "SIZE: " << vin.size() << "\n";
-    std::cout << "LEAVES: " << vin.leafCount() << "\n";
-    std::cout << "HEIGHT: " << vin.height() << "\n";
-    std::cout << "WIDTH: " << vin.width() << "\n";
-
-    list.clear();
-    vin.inorder(list);
-
-    for (double d : list) {
-        std::cout << d << " ";
-    }
-    std::cout << "\n";
-
+    demoThree();
 
     return 0;
+}
+
+void enterTestData(BinaryTree & bin) {
+    std::string testData =
+        "72.8 39.2 10.9 20.3 42.7 "
+        "40.0 69.8 50.2 73.2 90.2 "
+        "85.3 97.6 95.4";
+
+    std::stringstream ss(testData);
+
+    double temp;
+    while (ss >> temp) {
+        bin.insert(temp);
+    }
+}
+
+void displayTreeInfo(BinaryTree * const bin, std::string name) {
+    std::cout << "  " << name << " Information: \n";
+    std::vector<double> dList;
+    bin->inorder(dList);
+    std::cout << "    Data: ";
+    for (double d : dList) {
+        std::cout << d << " ";
+    }
+    std::cout << "\n";
+    std::cout << "    Size: " << bin->size() << "; ";
+    std::cout << "Leaf Count: " << bin->leafCount() << "; ";
+    std::cout << "Height: " << bin->height() << "; ";
+    std::cout << "Width: " << bin->width() << "\n";
+    std::cout << "\n";
+}
+
+void demoOne() {
+    BinaryTree treeOne;
+    enterTestData(treeOne);
+
+    std::vector<double> numList;
+    treeOne.inorder(numList);
+
+    std::cout << "Displaying list values in order: \n";
+    for (double d : numList) {
+        std::cout << d << " ";
+    }
+    std::cout << "\n\n";
+
+    std::cout << "Tree Information: \n";
+    std::cout << "  Size       : " << treeOne.size() << "\n"; // should be 13
+    std::cout << "  Leaf Count : " << treeOne.leafCount() << "\n"; // should be 5
+    std::cout << "  Height     : " << treeOne.height() << "\n"; // should be 5
+    std::cout << "  Width      : " << treeOne.width() << "\n"; // should be 5
+    std::cout << "\n";
+
+    double positiveTests[] = { 73.2, 42.7, 20.3, 50.2, 95.4 };
+    std::cout << "Testing for false-negatives (method should return true)... \n";
+    for (double test : positiveTests) {
+        std::cout << "  Searching for " << test << "... ";
+        if (treeOne.search(test)) {
+            std::cout << "Returned true. PASS. \n";
+        } else {
+            std::cout << "Returned false. FAIL. \n";
+        }
+    }
+    std::cout << "\n";
+
+    double negativeTests[] = { 31.4, 62.5, 99.9, 15.4, 62.7 };
+    std::cout << "Testing for false-positives (method should return false)... \n";
+    for (double test : negativeTests) {
+        std::cout << "  Searching for " << test << "... ";
+        if (treeOne.search(test)) {
+            std::cout << "Returned true. FAIL. \n";
+        } else {
+            std::cout << "Returned false. PASS. \n";
+        }
+    }
+}
+
+void demoTwo() {
+    BinaryTree treeOne;
+    enterTestData(treeOne);
+
+    std::vector<double> numList;
+    treeOne.inorder(numList);
+    BinaryTree treeTwo = treeOne; // Uses the copy constructor
+
+    std::cout << "Displaying initial states. \n"
+                 "They should have the same information. \n\n";
+    displayTreeInfo(&treeOne, "Initial TreeOne");
+    displayTreeInfo(&treeTwo, "Initial TreeOne");
+
+    double test[] = { 6.9, 80.2, 49.3 };
+    for (double d : test) {
+        treeTwo.insert(d);
+    }
+    std::cout << "Displaying post-addition information. \n\n"
+                 "They should be different. \n";
+    displayTreeInfo(&treeOne, "Post-Addition TreeOne");
+    displayTreeInfo(&treeTwo, "Post-Addition TreeTwo");
+
+}
+
+void demoThree() {
+    BinaryTree treeOne, treeTwo, treeThree;
+    enterTestData(treeOne);
+
+    std::vector<double> numList;
+    treeOne.inorder(numList);
+
+    std::cout << "Displaying initial states. \n"
+                 "Only treeOne should have members. \n\n";
+    displayTreeInfo(&treeOne, "TreeOne");
+    displayTreeInfo(&treeTwo, "TreeTwo");
+    displayTreeInfo(&treeThree, "ThreeTree");
+
+    treeThree = treeTwo = treeOne; // this uses an assignment operator.
+
+    std::cout << "Displaying post-assignment operator. \n"
+                 "The trees should all have the same information. \n\n";
+    displayTreeInfo(&treeOne, "TreeOne");
+    displayTreeInfo(&treeTwo, "TreeTwo");
+    displayTreeInfo(&treeThree, "ThreeTree");
+
+    // Adding stuff to treeTwo.
+    double testTwo[] = { 6.9, 80.2, 49.3 };
+    for (double d : testTwo) {
+        treeTwo.insert(d);
+    }
+
+    double testThree[] = { 108.2, 221.7, 397.8, 407.2, 503.4, 10.8, 21.6 };
+    treeThree = BinaryTree(); // Resetting treeTree
+    for (double d : testThree) {
+        treeThree.insert(d);
+    }
+
+    std::cout << "Displaying post-adjustment information. \n\n"
+                 "They should be different. \n";
+    displayTreeInfo(&treeOne, "TreeOne");
+    displayTreeInfo(&treeTwo, "TreeTwo");
+    displayTreeInfo(&treeThree, "ThreeTree");
 }
